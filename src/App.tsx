b3b0e35c470120505599518d2dd80141e8e7ef21
906bc906 <906bc906@gmail.com>
2022-10-04 16:40:01 +0900

    Building User Interfaces with Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import type { Component } from 'solid-js';
+import { BookList } from "./BookList"
+import { AddBook } from "./AddBook"
 
-const App: Component = () => {
+interface BookshelfProps {
+  name: string
+}
+
+function Bookshelf(props: BookshelfProps) {
   return (
     <div>
-      Hello, World
+      <h1>{props.name}'s Bookshelf</h1>
+      <BookList />
+      <AddBook />
     </div>
+  )
+}
+
+function App() {
+  return (
+    <Bookshelf name="solid"/>
   );
-};
+}
 
 export default App;
